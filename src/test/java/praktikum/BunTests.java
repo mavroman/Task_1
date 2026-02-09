e548747e559f2dc90567b3e084ec622aa2238d53
Mavroman <roman.it.qa@gmail.com>
2026-02-09 21:33:20 +0300

    edit BunTests

--- src/test/java/praktikum/BunTests.java
+++ src/test/java/praktikum/BunTests.java
@@ -1,17 +1,12 @@
 package praktikum;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+
 @DisplayName("Класс Bun - модель булочки для бургера")
 public class BunTests {
 
@@ -29,23 +24,6 @@ public class BunTests {
 
         assertEquals(name, bun.getName(), "Название булочки должно совпадать");
         assertEquals(price, bun.getPrice(), "Цена булочки должна совпадать");
-    }
-
-
-    @Test
-    @DisplayName("Мок объекта Bun должен возвращать стаб значения")
-    void mock_MustReturnStubValues() {
-        Bun mockBun = mock(Bun.class);
-
-        when(mockBun.getName()).thenReturn("Булочка с моком");
-        when(mockBun.getPrice()).thenReturn(250f);
-
-        String actualName = mockBun.getName();
-        float actualPrice = mockBun.getPrice();
-
-        assertEquals("Булочка с моком", actualName);
-        assertEquals(250f, actualPrice);
-
     }
 
 
